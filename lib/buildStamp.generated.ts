44b5390e89a856102e620c420ce6e8610d962923
wikipeterson <steve@wikipeterson.org>
2026-05-11 08:11:04 -0400

    Enlarge Queens puzzle markers

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-05-11T12:07:35.368Z"
-export const BUILD_COMMIT = "045c925"
+export const BUILD_STAMP_ISO = "2026-05-11T12:10:23.113Z"
+export const BUILD_COMMIT = "e972a19"

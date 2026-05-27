7f88eddc62ce22303f1bd8f934ad5ff08360926d
wikipeterson <steve@wikipeterson.org>
2026-05-27 12:10:06 -0400

    Rename lounge builder massage region

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-05-27T15:44:16.899Z"
-export const BUILD_COMMIT = "5509f54"
+export const BUILD_STAMP_ISO = "2026-05-27T16:07:24.544Z"
+export const BUILD_COMMIT = "77847fe"

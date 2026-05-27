045faf1fb9f3788f3824e09b446500926d4c4b7b
wikipeterson <steve@wikipeterson.org>
2026-05-27 09:49:58 -0400

    Add lounge builder teacher toggle

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-05-27T13:23:56.849Z"
-export const BUILD_COMMIT = "cb8bc4d"
+export const BUILD_STAMP_ISO = "2026-05-27T13:43:09.710Z"
+export const BUILD_COMMIT = "12b9222"

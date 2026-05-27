9426fd0619d16a80bcfbfe823196fb7b7c66c267
wikipeterson <steve@wikipeterson.org>
2026-05-27 14:49:10 -0400

    Add lounge builder grid size control

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-05-27T17:53:57.602Z"
-export const BUILD_COMMIT = "9cf3921"
+export const BUILD_STAMP_ISO = "2026-05-27T18:34:13.139Z"
+export const BUILD_COMMIT = "bd40661"

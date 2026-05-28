96b9f6779c6df529b7d8fd45a890cc6fcb5a4e08
wikipeterson <steve@wikipeterson.org>
2026-05-28 10:19:40 -0400

    Allow teachers on lounge yoga mats

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-05-28T14:00:55.822Z"
-export const BUILD_COMMIT = "6d11995"
+export const BUILD_STAMP_ISO = "2026-05-28T14:09:44.340Z"
+export const BUILD_COMMIT = "2f99b20"

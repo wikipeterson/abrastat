33c8634991c48d4d4dd9690938fde288f25436a0
wikipeterson <steve@wikipeterson.org>
2026-06-01 09:58:00 -0400

    Show Scrabble intersection letters

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-06-01T13:50:21.905Z"
-export const BUILD_COMMIT = "90a9a86"
+export const BUILD_STAMP_ISO = "2026-06-01T13:56:57.820Z"
+export const BUILD_COMMIT = "b22da49"

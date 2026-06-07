e9089116579cab6c3572b645872dd2933f876d6f
wikipeterson <steve@wikipeterson.org>
2026-06-07 15:28:07 -0400

    Reduce one-prop setup typography

--- lib/buildStamp.generated.ts
+++ lib/buildStamp.generated.ts
@@ -1,2 +1,2 @@
-export const BUILD_STAMP_ISO = "2026-06-07T18:29:20.421Z"
-export const BUILD_COMMIT = "7bf9c03"
+export const BUILD_STAMP_ISO = "2026-06-07T19:25:51.425Z"
+export const BUILD_COMMIT = "7263694"
